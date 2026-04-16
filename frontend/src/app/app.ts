ca00bcd596e58cf0f126be8994c08d0e7ec3c02f
Markus Reichl <markus@re1.at>
2026-04-16 16:19:25 +0200

    Require Keycloak login for Angular

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { keycloak } from '../main';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  title = signal('stranger');
+
+  ngOnInit() {
+    keycloak.loadUserProfile().then((profile) => {
+      this.title.set(profile.firstName!);
+    });
+  }
 }
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import Keycloak from 'keycloak-js';
 
-bootstrapApplication(App, appConfig)
+export const keycloak = new Keycloak({
+  url: 'http://localhost:8888',
+  realm: 'tutorial-realm',
+  clientId: 'tutorial-client',
+});
+
+keycloak
+  .init({ onLoad: 'login-required' })
+  .then(() => bootstrapApplication(App, appConfig))
   .catch((err) => console.error(err));
