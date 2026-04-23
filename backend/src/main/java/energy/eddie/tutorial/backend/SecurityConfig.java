cf68368733d7fe549a913e45d05b0ced717eccbe
Markus Reichl <markus@re1.at>
2026-04-23 11:40:18 +0200

    Set csrf and ddl update options

--- backend/src/main/java/energy/eddie/tutorial/backend/SecurityConfig.java
+++ backend/src/main/java/energy/eddie/tutorial/backend/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,6 +14,7 @@ class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http
                 .cors(Customizer.withDefaults())
+                .csrf(CsrfConfigurer::spa)
                 .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
